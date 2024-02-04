215d2fa04b71347058fccfb660f27a7ec8925cc0
전영서 <20wjsdudtj@gmail.com>
2024-02-04 19:11:38 +0900

    프로젝트 init

---
+++ KoalaAuth/src/main/java/com/example/koalaauth/KoalaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.koalaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoalaAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoalaAuthApplication.class, args);
+    }
+
+}
---
+++ KoalaAuth/src/test/java/com/example/koalaauth/KoalaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koalaauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoalaAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
