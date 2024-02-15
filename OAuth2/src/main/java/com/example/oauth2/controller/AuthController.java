8d77756d42449ce6f5a621ea41b9aac0a5c2e92b
youngseojeon <20wjsdudtj@gmail.com>
2024-02-15 16:58:00 +0900

    MVC 패턴으로 인증 처리 예정

---
+++ OAuth2/src/main/java/com/example/oauth2/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.example.oauth2.controller;
+
+import com.example.oauth2.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/auth")
+public class AuthController {
+
+    /* TODO
+    로그인 / 로그아웃 구현
+     */
+}
---
+++ OAuth2/src/main/java/com/example/oauth2/service/AuthService.java
@@ -0,0 +1,11 @@
+package com.example.oauth2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    /* TODO
+    애플/카카오 로그인과 관련한 서비스 로직 구현
+     */
+}
