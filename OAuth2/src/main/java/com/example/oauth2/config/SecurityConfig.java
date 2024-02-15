43ce648047d8e2e83de1b3647bef929d2950009a
youngseojeon <20wjsdudtj@gmail.com>
2024-02-15 16:02:30 +0900

    UsernamePasswordAuthenticationFilter 전에 CustomFilter를 수행하기 위한 코드 추가 /home, /login url에 대해서는 모든 요청 로그인 없이 가능

--- OAuth2/src/main/java/com/example/oauth2/config/SecurityConfig.java
+++ OAuth2/src/main/java/com/example/oauth2/config/SecurityConfig.java
@@ -1,9 +1,27 @@
 package com.example.oauth2.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    /* TODO
-     * 권한, 비밀번호 인코딩/디코딩 설정 등 FilterChain에 대한 설정 필요
-     *
-     */
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                //Websecurity 설정 필요
+                .authorizeHttpRequests((authorize)->authorize
+                        .requestMatchers("/home", "/login").permitAll()
+                        .anyRequest().authenticated())
+                // ...
+                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class); //UsernamePasswordAuthenticationFilter 전에 수행할 것이다.
+
+        return http.build();
+
+    }
+
+
 }
