b4dc633c5d7865c50ed32a1646351184c2f3b420
youngseojeon <20wjsdudtj@gmail.com>
2024-02-15 15:51:08 +0900

    JwtAuthentication TODO 추가 Authorization 헤더에 OAuth2.9 Bearer Token을 넣기 위해 OncePerRequestFilter를 확장해 custom filter 생성 예정

--- OAuth2/src/main/java/com/example/oauth2/config/JwtAuthenticationFilter.java
+++ OAuth2/src/main/java/com/example/oauth2/config/JwtAuthenticationFilter.java
@@ -10,13 +10,24 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    /*
+    인증 Header에 특정한 형식(Oauth2.0 Bearer Token)을 넣기 위해 CustomFilter 추가
+    Filter 인터페이스 구현 대신 OncePerRequestFilter를 확장한다.
+     */
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        /*
+        1. Request Header 에서 토큰을 꺼냄
+        2. validateToken 으로 토큰 유효성 검사
+          ->  정상 토큰이면 해당 토큰으로 Authentication 을 가져와서 SecurityContext 에 저장
+        3. 토큰을 꺼낸 후 토큰 속 정보를 꺼내오기 (여기에서 할지 TokenService에서 할지 결정할 것 !)
+         */
+
     }
-    /* TODO
-     * CustomFilter를 만들어
-     * 기존 UsernamePasswordAuthenticationFilter보다 먼저 걸릴 수 있도록 설정해주어야 한다.
-     * OncePerRequestFilter 확장
-     */
 }
